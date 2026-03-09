6f413319adb12689e47b6f9993b3ef5a3323e9eb
Thomas Yang <thomas.yang2@gmail.com>
2026-03-10 10:03:56 +1100

    format

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ module "stack_us" {
 
   source = "./modules/regional_stack"
 
-  region           = "us-east-1"
-  cognito_pool_arn = module.cognito.pool_arn
-  email            = var.email
-  repo_url         = var.repo_url
-  sns_topic        = var.sns_topic
-  cognito_user_pool_id = module.cognito.cognito_user_pool_id
+  region                      = "us-east-1"
+  cognito_pool_arn            = module.cognito.pool_arn
+  email                       = var.email
+  repo_url                    = var.repo_url
+  sns_topic                   = var.sns_topic
+  cognito_user_pool_id        = module.cognito.cognito_user_pool_id
   cognito_user_pool_client_id = module.cognito.cognito_user_pool_client_id
 
   providers = {
@@ -25,12 +25,12 @@ module "stack_eu" {
 
   source = "./modules/regional_stack"
 
-  region           = "eu-west-1"
-  cognito_pool_arn = module.cognito.pool_arn
-  email            = var.email
-  repo_url         = var.repo_url
-  sns_topic        = var.sns_topic
-  cognito_user_pool_id = module.cognito.cognito_user_pool_id
+  region                      = "eu-west-1"
+  cognito_pool_arn            = module.cognito.pool_arn
+  email                       = var.email
+  repo_url                    = var.repo_url
+  sns_topic                   = var.sns_topic
+  cognito_user_pool_id        = module.cognito.cognito_user_pool_id
   cognito_user_pool_client_id = module.cognito.cognito_user_pool_client_id
 
   providers = {
