b713eff6d505814138d20a5e087238be95eca023
Thomas Yang <thomas.yang2@gmail.com>
2026-03-14 08:17:54 +1100

    network mode

--- modules/regional_stack/ecs.tf
+++ modules/regional_stack/ecs.tf
@@ -15,7 +15,7 @@ resource "aws_ecs_task_definition" "dispatch_task" {
   cpu    = "256"
   memory = "512"
 
-  network_mode = "awsvpc"
+  network_mode = "bridge"
 
   execution_role_arn = aws_iam_role.ecs_task_execution.arn
   task_role_arn      = aws_iam_role.ecs_task_execution.arn
@@ -36,101 +36,6 @@ resource "aws_ecs_task_definition" "dispatch_task" {
       ]
     }
   ])
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_flow_log" "example" {
-  iam_role_arn    = aws_iam_role.example.arn
-  log_destination = aws_cloudwatch_log_group.example.arn
-  traffic_type    = "ALL"
-  vpc_id          = aws_vpc.main.id
-}
-
-resource "aws_cloudwatch_log_group" "example" {
-  name = "example"
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["vpc-flow-logs.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name               = "example"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-data "aws_iam_policy_document" "example" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "logs:DescribeLogGroups",
-      "logs:DescribeLogStreams",
-    ]
-
-    resources = ["*"]
-  }
-}
-
-resource "aws_iam_role_policy" "example" {
-  name   = "example"
-  role   = aws_iam_role.example.id
-  policy = data.aws_iam_policy_document.example.json
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-west-1a"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route" "internet" {
-  route_table_id         = aws_route_table.public.id
-  gateway_id             = aws_internet_gateway.igw.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
-resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_security_group" "ecs_task" {
-  name        = "ecs-task-sg"
-  vpc_id      = aws_vpc.main.id
-  description = "Allow outbound traffic to SNS"
-
-  egress {
-    description = "Allow outbound HTTPS traffic"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 output "ecs_cluster" {
